bbfec9d5ee0df301acfe00c25c0474ef254a9b0a
dalya-kh <dalyakharabsheh@gmail.com>
2024-03-09 08:51:34 +0300

    first update

---
+++ src/JavaClassPackage/JavaClass.java
@@ -0,0 +1,21 @@
+package JavaClassPackage;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class JavaClass {
+
+	@BeforeTest
+	public void MyBeforeTest() {
+
+	}
+
+	@Test
+
+	public void MyTest() {
+		String MyName = "dalya";
+		System.out.println(MyName.contains("d"));
+
+	}
+
+}
